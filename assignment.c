e9abf0d53fcb83eb119e3669931450e6e7af235b
AdelekeAP <aaladenusi@gmail.com>
2024-11-12 00:32:46 +0100

    assigment on switch statements

--- assignment.c
+++ assignment.c
@@ -1,62 +1,62 @@
 #include <stdio.h>
 
-
 int main() {
-    char choice;
+    char type;
     printf("Enter 'c' for character, 'f' for float, or 'i' for integer: ");
-    scanf(" %c", &choice);
+    scanf(" %c", &type);
 
-    switch (choice) {
+    switch (type) {
         case 'c': {
-            char input;
+            char ch;
             printf("Enter a character: ");
-            scanf(" %c", &input);
+            scanf(" %c", &ch);
+            printf("\nCharacter: %c\n", ch);
+            printf("ASCII code: %d\n", ch);
+            printf("Size: %lu bytes\n", sizeof(ch));
 
-            printf("\nCharacter: %c\n", input);
-            printf("ASCII code: %d\n", input);
-            printf("Size of character: %lu bytes\n\n", sizeof(input));
-
-            printf("Next four characters in multiples of 3:\n");
+            printf("\nCharacter calculations:\n");
             for (int i = 1; i <= 4; i++) {
-                char nextChar = input + (i * 3);
-                printf("Character: %c, ASCII code: %d\n", nextChar, nextChar);
+                char nextChar = ch + (3 * i);
+                printf("Character: %c, ASCII: %d, Size: %lu bytes\n", nextChar, (int)nextChar, sizeof(nextChar));
             }
             break;
         }
+
         case 'f': {
-            float input;
+            float num;
             printf("Enter a float: ");
-            scanf("%f", &input);
+            scanf("%f", &num);
+            printf("\nFloat: %.2f\n", num);
+            printf("Size of float: %lu bytes\n\n", sizeof(num));
 
-            printf("\nFloat: %.2f\n", input);
-            printf("Size of float: %lu bytes\n\n", sizeof(input));
-
-            printf("Next four floats in multiples of 3:\n");
+            printf("\nNext 4 Float values:\n");
             for (int i = 1; i <= 4; i++) {
-                float nextFloat = input + (i * 3);
-                printf("Float: %.2f\n", nextFloat);
+                float nextFloat = num * (3.0f * i);
+                printf("Float: %.2f, Size: %lu bytes\n", nextFloat, sizeof(nextFloat));
             }
             break;
         }
+
         case 'i': {
-            int input;
+            int num;
             printf("Enter an integer: ");
-            scanf("%d", &input);
+            scanf("%d", &num);
+            printf("\nInteger: %d\n", num);
+            printf("Size of integer: %lu bytes\n\n", sizeof(num));
 
-            printf("\nInteger: %d\n", input);
-            printf("Size of integer: %lu bytes\n\n", sizeof(input));
-
-            printf("Next four integers in multiples of 3:\n");
+            printf("\nNext 4 Integer values:\n");
             for (int i = 1; i <= 4; i++) {
-                int nextInt = input + (i * 3);
-                printf("Integer: %d\n", nextInt);
+                int nextInt = num * (3 * i);
+                printf("Integer: %d, Size: %lu bytes\n", nextInt, sizeof(nextInt));
             }
             break;
         }
+
         default:
-            printf("Invalid choice. Please enter 'c', 'f', or 'i'.\n");
-            break;
+            printf("Invalid input type.\n");
     }
 
     return 0;
 }
+
+
